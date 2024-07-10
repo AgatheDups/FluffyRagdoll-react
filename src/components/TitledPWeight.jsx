1d26e52165e5a5c2945f943c1bf38482f3d34259
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-07-10 12:20:15 +0200

    Add component for weightCalculator and change css

---
+++ src/components/TitledPWeight.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+
+export default function TitledPWeight ({title,children}){
+    return <div>
+        <h5>{title}</h5>
+        {children}
+    </div>
+}
--- src/page/WeightCaculator.jsx
+++ src/page/WeightCaculator.jsx
@@ -1,5 +1,6 @@
 import TitledP from "../components/TitledP";
 import Footer from "../components/Footer";
+import TitledPWeight from "../components/TitledPWeight";
 
 
 export default function WeightCaculator (){
@@ -19,7 +20,12 @@ export default function WeightCaculator (){
                         <p>Poids pour un femelle</p>
                     </div>
                     <br/>
-                    <p>La croissance du Ragdolls se fait au long de ses deux premières années, donc après ses 2ans votre Ragdoll doit se strabiliser.</p>
+                    <TitledPWeight title={"À la naissance et à 3 mois"}>
+                        <p>{"Le poids de naissance de cette race varie généralement entre 80 et 130 grammes. Le sexe des chatons influence également leur poids. À trois mois, les femelles pèsent entre 1,2 et 1,63 kg, tandis que les mâles peuvent atteindre 2,3 kg. Cependant, il est essentiel de noter qu'il n'existe pas de règle absolue en matière de croissance."}</p>
+                    </TitledPWeight>
+                    <TitledPWeight title={"À plus d'un an et l'âge adulte"}>
+                    <p>{"Ce félin au caractère docile pèse entre 3,5 et 7 kg à l'âge d'un an. La femelle pèse généralement entre 3,5 et 5,4 kg, tandis que le mâle se situe entre 6 et 7,7 kg. À l'âge adulte, un Ragdoll peut peser de 8 à 10 kg. Les femelles atteignent souvent un poids maximal de 8 kg, et certains mâles peuvent même atteindre jusqu'à 17 kg."}</p>
+                    </TitledPWeight>
                 </div>
             </div>
             <Footer/> 

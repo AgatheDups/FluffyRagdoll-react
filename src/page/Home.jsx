5d61398bb8999fb124acabc186509471d395bf60
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-07-15 14:03:31 +0200

    change tab

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -8,22 +8,22 @@ import { motion } from "framer-motion"
 import { useEffect, useState } from "react";
 
 export default function Home (){
-        // Text, pagragraph, Food and Toy card Json
-        const [imagedTitleP, setImagedTitleP] = useState({});
-        const [titleP, setTitleP] = useState({});
-        const [paragraphsOrigin, setParagraphsOrigin] = useState([]);
-        const [paragraphsPhysical, setParagraphsPhysical] = useState([]);
-        const [paragraphsCharacter, setParagraphsCharacter] = useState([]);
-        const [paragraphsExpectancy, setParagraphsExpectancy] = useState([]);
-    
-        useEffect(() => {
-            setImagedTitleP(homeData.imagedTitleP);
-            setTitleP(homeData.titleP);
-            setParagraphsOrigin(homeData.origin);
-            setParagraphsPhysical(homeData.physical);
-            setParagraphsCharacter(homeData.character); 
-            setParagraphsExpectancy(homeData.bubble); 
-        },[]);
+    // Text, pagragraph, Food and Toy card Json
+    const [imagedTitleP, setImagedTitleP] = useState({});
+    const [titleP, setTitleP] = useState({});
+    const [paragraphsOrigin, setParagraphsOrigin] = useState([]);
+    const [paragraphsPhysical, setParagraphsPhysical] = useState([]);
+    const [paragraphsCharacter, setParagraphsCharacter] = useState([]);
+    const [paragraphsExpectancy, setParagraphsExpectancy] = useState([]);
+
+    useEffect(() => {
+        setImagedTitleP(homeData.imagedTitleP);
+        setTitleP(homeData.titleP);
+        setParagraphsOrigin(homeData.origin);
+        setParagraphsPhysical(homeData.physical);
+        setParagraphsCharacter(homeData.character); 
+        setParagraphsExpectancy(homeData.bubble); 
+    },[]);
 
     return (
         <motion.div initial={{opacity:0}} animate={{opacity:1}}>

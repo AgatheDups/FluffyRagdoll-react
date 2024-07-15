3330969c86d5e2469a9178e21058bb7b70bc3bf4
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-07-15 15:14:08 +0200

    add json in SignUp

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
+import signUpData from "./json/signup.json"
 import { motion } from "framer-motion"
 
 const isEmail = (email) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email.toUpperCase());
 
 export default function SignUp (){
+    // placeholder email, placeholder password and button in Json
+    const [contentSignUp, setContentSignUp] = useState({});
+
+
+    useEffect(() => {
+        setContentSignUp(signUpData);
+    },[]);
+
+
     // function to change invalid icon to valid for email 
     const [values, setValues] = useState({ email: "", emailVerif: "" });
     const [errors, setErrors] = useState({});
@@ -62,29 +72,29 @@ export default function SignUp (){
                                     <div>
                                         <div className="box d-flex">
                                             <label htmlFor="email" className="form-label"/>
-                                            <input type="email" className="form-control" value={values.email} onChange={setEmail} id="email" placeholder="Email" required/>
+                                            <input type="email" className="form-control" value={values.email} onChange={setEmail} id="email" placeholder={contentSignUp.pEmail} required/>
                                             <div className="valid"><ion-icon name={isEmail(values.email) ? "checkmark-outline" : "close-outline"}></ion-icon></div>
                                         </div>
                                         {errors.email && <div className="text-danger">{errors.email}</div>}
                                     </div>
                                     <div className="box d-flex">
                                         <label htmlFor="email-verif" className="form-label"/>
-                                        <input type="email" className="form-control email" value={values.emailVerif} onChange={setEmailVerif} id="email-verif" placeholder="Vérification email" required/>
+                                        <input type="email" className="form-control email" value={values.emailVerif} onChange={setEmailVerif} id="email-verif" placeholder={contentSignUp.pEmailVerif} required/>
                                         <span className="valid-verif"><ion-icon name={values.emailVerif == values.email && values.emailVerif != "" ? "checkmark-outline" : "close-outline"}></ion-icon></span>    
                                     </div>
                                     {errors.emailVerif && <div className="text-danger">{errors.emailVerif}</div>}
                                     <div className="box d-flex">
                                         <label htmlFor="password" className="form-label"/>
-                                        <input type={passwordVisible ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="password" placeholder="Mot de passe" required/>
+                                        <input type={passwordVisible ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="password" placeholder={contentSignUp.pPassword} required/>
                                         <span className="showPassword" onClick={toggleVisibility}><ion-icon name={passwordVisible ? "eye-outline" : "eye-off-outline"}></ion-icon></span>
                                     </div>
                                     <div className="mb-3 box d-flex">
                                         <label htmlFor="password" className="form-label"/>
-                                        <input type={passwordVerifVisible ? 'text' : 'password'} value={passwordVerif} onChange={(e) => setPasswordVerif(e.target.value)} className="form-control" id="password-verif" placeholder="Vérification mot de passe" required/>
+                                        <input type={passwordVerifVisible ? 'text' : 'password'} value={passwordVerif} onChange={(e) => setPasswordVerif(e.target.value)} className="form-control" id="password-verif" placeholder={contentSignUp.pPasswordVerif} required/>
                                         <span className="showPassword-verif"onClick={toggleVisibilityVerif}><ion-icon name={passwordVerifVisible ? "eye-outline" : "eye-off-outline"}></ion-icon></span>
                                     </div>
                                     <div className="d-flex justify-content-end">
-                                    <button type="submit" className="btn btn-danger">Connexion</button>
+                                    <button type="submit" className="btn btn-danger">{contentSignUp.buttonConnection}</button>
                                     </div>
                                 </form>
                             </div>

b8972c939f4ece11c185b1bd9be9c38903d6be25
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-07-15 13:55:33 +0200

    add json in NotFound

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
+import NotFoundData from "./json/notfound.json"
 import { motion } from "framer-motion"
 
 
 export default function NotFound(){
+    const [h2, setH2] = useState({});
+
+    useEffect(() => {
+        setH2(NotFoundData);
+    },[]);
+
+
     return (
         <motion.div initial={{opacity:0}} animate={{opacity:1}}>
             <div id="content-panel">
-                <h2>Page non trouvée</h2>
+                <h2>{h2.content}</h2>
                 <img className="image-chat col-lg-4" id="image1" src="../public/image/404.jpg" alt="Erreur 404" />
             </div>
             <Footer classFooter={'stick-footer'}/> 

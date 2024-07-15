5f88941d763e5be75f4b9556cefa3c22781e5509
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-07-15 14:26:09 +0200

    add json SignIn

--- src/page/SignIn.jsx
+++ src/page/SignIn.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable react/prop-types */
 import { NavLink } from "react-router-dom";
 import Footer from "../components/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import signInData from "./json/signin.json"
 import { motion } from "framer-motion"
 
+// regex for email
 const isEmail = (email) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email.toUpperCase());
 
 export default function SignIn (){
+    // placeholder email and password, button and links in Json
+    const [contentSignIn, setContentSignIn] = useState({});
+
+
+    useEffect(() => {
+        setContentSignIn(signInData);
+    },[]);
+
+
     // function to change invalid icon to valid for email 
     const [values, setValues] = useState({ email: "" });
     const [errors, setErrors] = useState({});
@@ -40,7 +51,7 @@ export default function SignIn (){
         setPasswordVisible(!passwordVisible);
     };
 
-    // 
+    // content for sign in
     return (
         <motion.div initial={{opacity:0}} animate={{opacity:1}}>
             <div id="content-panel-card">
@@ -51,7 +62,7 @@ export default function SignIn (){
                                 <form onSubmit={validateAndSubmitForm} action="" method="post">
                                     <div className="box d-flex">
                                         <label htmlFor="email" className="form-label"/>
-                                        <input type="email" className="form-control" id="email" value={values.email} onChange={setEmail} placeholder="Email" required/>
+                                        <input type="email" className="form-control" id="email" value={values.email} onChange={setEmail} placeholder={contentSignIn.pEmail} required/>
                                         <div className="valid">
                                             <ion-icon name={isEmail(values.email) ? "checkmark-outline" : "close-outline"}/>
                                         </div>
@@ -59,18 +70,18 @@ export default function SignIn (){
                                     {errors.email && <div className="text-danger">{errors.email}</div>}
                                     <div className="mb-3 box d-flex">
                                         <label htmlFor="password" className="form-label"/>
-                                        <input type={passwordVisible ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="password" placeholder="Mot de passe" required/>
+                                        <input type={passwordVisible ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="password" placeholder={contentSignIn.pPassword} required/>
                                         <div className="showPassword" onClick={toggleVisibility}>
                                             <ion-icon name={passwordVisible ? "eye-outline" : "eye-off-outline"}/>
                                         </div>
                                     </div>
                                     <div className="d-flex justify-content-end">
-                                    <button type="submit" className="btn btn-danger">Connexion</button>
+                                    <button type="submit" className="btn btn-danger">{contentSignIn.buttonConnection}</button>
                                     </div>
                                     <div>
-                                    <NavLink to="/motdepasse-oublier">Mot de passe oublié ?</NavLink>
+                                    <NavLink to="/motdepasse-oublier">{contentSignIn.linkLostP}</NavLink>
                                     <br/>
-                                    <NavLink to="/inscription">Pas de compte ?</NavLink>
+                                    <NavLink to="/inscription">{contentSignIn.linkSignUp}</NavLink>
                                     </div>
                                 </form>
                             </div>

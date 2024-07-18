4d8b5d2d5fcdee9fc97fc4ad32dbb18dfaecdaa2
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-07-18 09:14:32 +0200

    add two button for change language

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,5 +3,13 @@
 export default function Footer ({classFooter}){
     return <footer className={classFooter}>
         <p>© Agathe Dupuis 2024</p>
+        <div className="div-language">
+            <button className="btn-language">
+                <img className="img-language" src="/image/drapeau-france.png" alt="" />
+            </button>
+            <button className="btn-language">
+                <img className="img-language" src="/image/drapeau-R-U.png" alt="" />
+            </button>
+        </div>
     </footer>
 }

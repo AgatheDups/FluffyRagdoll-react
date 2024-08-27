a224521f428dc0da4821626970d3edbd8f09cf25
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-08-27 16:00:49 +0200

    Change send mail

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,12 +1,35 @@
+/* eslint-disable react/prop-types */
 import { motion } from "framer-motion"
 import { getContactData } from "../InternationalIzer";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 import Footer from "../components/Footer";
 
 export default function Contact (){
         // Call Json
     // eslint-disable-next-line no-unused-vars
     const [contactData, _] = useState(getContactData())
+    const form = useRef();
+    const [mailSent, setMailSent] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        if(mailSent){
+            return
+        }
+        emailjs
+        .sendForm('service_6yjr2vm', 'template_io72mpb', form.current, {
+          publicKey: 'ZlnVL4Dadk8igzUm9',
+        })
+        .then(
+          () => {
+            setMailSent(true)
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
 
     return (
         <motion.div initial={{opacity:0}} animate={{opacity:1}}>
@@ -16,22 +39,22 @@ export default function Contact (){
                         <div className="card bg-light mb-4 custom-shadow">
                             <div className="card-body card-contact">
                             <h6>{contactData.title}</h6>
-                                <form action="">
+                                <form ref={form} onSubmit={sendEmail}>
                                     <div className="box d-flex box-contact">
                                         <label htmlFor="name"/>
-                                        <input type="text" placeholder={contactData.name}/>
+                                        <input type="text" id="name" name="from_name" placeholder={contactData.name} required/>
                                     </div>
                                     <div className="box d-flex box-contact">
                                         <label htmlFor="email"/>
-                                        <input type="text"  placeholder={contactData.email}/>
+                                        <input type="email" id="email" name="from_mail"  placeholder={contactData.email} required/>
                                     </div>
                                     <div className="box d-flex box-contact">
                                         <label htmlFor="subject"/>
-                                        <input type="text"  placeholder={contactData.subject}/>
+                                        <input type="text" id="subject" name="subject" placeholder={contactData.subject} required/>
                                     </div>
                                     <div className="box d-flex box-contact">
                                         <label htmlFor="message"/>
-                                        <textarea type="text"  placeholder={contactData.message}/> 
+                                        <textarea type="text" id="message" name="message"  placeholder={contactData.message} required/> 
                                     </div>
                                 </form>
                                 <button type="submit" className="btn btn-danger">{contactData.buttonSubmit}</button>
